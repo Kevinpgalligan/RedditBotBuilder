866cc911235d12198e10605d3defa8ffb8a242c2
kevinpgalligan <galligankevinp@gmail.com>
2020-02-10 22:27:31 +0000

    A bit more of the MVP filled in.

--- src/rbb/bot.py
+++ src/rbb/bot.py
@@ -38,9 +38,33 @@ RUN_INTERVAL_SECONDS = 60
 # Prevents bot from getting stuck in infinite loops.
 POST_LIMIT_PER_SUBMISSION = 5
 
+def normalise(name):
+    return name.lower()
+
+def normalised_set(*names):
+    return set(map(normalise, names))
+
+BASE_USER_BLACKLIST = normalised_set(
+    "AutoModerator",
+    # Don't know if this bot is still active, but it annoyed the
+    # hell out of me by stalking AnEmojipastaBot.
+    "Sub_Corrector_Bot"
+)
+
+BASE_SUBREDDIT_BLACKLIST = normalised_set(
+    # There will always be sh*tbags who try to f*ck with people
+    # in these subreddits.
+    "SuicideWatch",
+    "depression"
+)
+
 class RedditBot:
-    def run(self):
-        runner = BotRunner(auth.reddit_from_program_args(), self)
+    def run(self, _bot_runner_factory_fn=BotRunner):
+        runner = _bot_runner_factory_fn(
+            auth.reddit_from_program_args(),
+            self,
+            BASE_USER_BLACKLIST,
+            BASE_SUBREDDIT_BLACKLIST)
         runner.start_loop()
 
     def reply_to_mention(self, username, text):
@@ -52,11 +76,8 @@ class RedditBot:
     def process_mention(self, comment):
         pass
 
-def normalise(name):
-    return name.lower()
-
 class BotRunner:
-    
+
     filters = [
         is_post,
         self.is_tagged_in,
@@ -65,15 +86,17 @@ class BotRunner:
         self.subreddit_is_blacklisted
     ]
 
-    def __init__(self, reddit, bot):
+    def __init__(self, reddit, bot, user_blacklist, subreddit_blacklist):
         self.reddit = reddit
         self.bot = bot
         self.inbox = reddit.inbox
         self.user = normalise(reddit.user.me().name)
         self.post_counter_by_submission = defaultdict(int)
+        self.user_blacklist = user_blacklist
+        self.subreddit_blacklist = subreddit_blacklist
 
-    def start_loop(self):
-        while True:
+    def start_loop(self, _loop_condition=always_true):
+        while _loop_condition():
             self.process_inbox()
             time.sleep(RUN_INTERVAL_SECONDS)
 
@@ -83,7 +106,7 @@ class BotRunner:
             # the same comment over and over due to a bug.
             self.inbox.mark_read([item])
             if self.passes_filters(item):
-                pass # TODO stuff
+                self.pass_to_bot(item)
                 # TODO increment post counter
 
     def passes_filters(self, item):
@@ -91,15 +114,31 @@ class BotRunner:
 
     def is_tagged_in(self, item):
         return self.user in item.body.lower()
-    
+
     def interacted_too_many_times_in_thread(self, item):
+        # TODO this is too restrictive...
         return self.post_counter_by_submission[item.link_id] > POST_LIMIT_PER_SUBMISSION
 
     def author_is_blacklisted(self, item):
-        pass # TODO
+        return author_name(item) in self.user_blacklist
 
     def subreddit_is_blacklisted(self, item):
-        pass # TODO
+        return subreddit_name(item) in self.subreddit_blacklist
+
+    def pass_to_bot(self, item):
+        # TODO call all of the bot's user-defined functions.
+        pass
+
+def always_true():
+    return True
 
 def is_post(item):
     return isinstance(item, Comment) or isinstance(item, Submission)
+
+def author_name(item):
+    # TODO check if this works for other item types, e.g. posts, messages...
+    return "[deleted]" if item.author is None else normalise(item.author)
+
+def subreddit_name(item):
+    # TODO make sure that this isn't called for messages 
+    return normalise(item.subreddit.display_name)
